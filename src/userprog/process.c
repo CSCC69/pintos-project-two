d83df5460a7ec30f3911f3d1aa81f0c0e65ab465
Nathanael Robbins <nejrobbins@gmail.com>
2024-02-16 22:10:07 -0500

    parse command line arguments for new process

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -38,6 +38,11 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
+  char *token, *save_ptr;
+  for (token = strtok_r (fn_copy, " ", &save_ptr); token != NULL;
+       token = strtok_r (NULL, " ", &save_ptr))
+    printf ("'%s'\n", token);
+
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
