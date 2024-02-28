c158a0b0aae6ad52b99a18e3a50c43f91e2a4d63
KingLouis-CLXXII <30333055+KingLouis-CLXXII@users.noreply.github.com>
2024-02-27 20:05:42 -0500

    oops

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -164,8 +164,7 @@ page_fault (struct intr_frame *f)
     return;
   } 
 
-  //TODO: maybe remove phys base check
-  if (fault_addr >= PHYS_BASE || (thread_current()->pagedir, fault_addr) == NULL)
+  if (fault_addr >= PHYS_BASE || pagedir_get_page(thread_current()->pagedir, fault_addr) == NULL)
    exit(-1);
 
   /* To implement virtual memory, delete the rest of the function
